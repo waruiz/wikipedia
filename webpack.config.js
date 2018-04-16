327ab83a2fe1c4dc2fd302856192c8909ca30e48
Will Ruiz <thisis.willruiz@gmail.com>
2018-04-15 23:27:18 -0700

    [fix] - Fix webpack to serve React files and understand ES6

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,29 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, 'client/src');
+const DIST_DIR = path.join(__dirname, '/client/public');
+
 module.exports = {
-  entry: ['babel-polyfill', '/client/src/index.js']
+  mode: 'production',
+  entry: ['babel-polyfill', `${SRC_DIR}/index.js`],
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        include: SRC_DIR,
+        use: [
+          {
+            loader: 'babel-loader',
+            query: {
+              presets: ['react'],
+            },
+          },
+        ]
+      },
+    ],
+  },
 };

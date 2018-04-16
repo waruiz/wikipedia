973df0f45e60eb3d61b9c923c52c1b84b90aa090
Will Ruiz <thisis.willruiz@gmail.com>
2018-04-15 23:26:20 -0700

    [ops] - Server serves content on port 1337

--- server/index.js
+++ server/index.js
@@ -6,4 +6,15 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: true } ) );
-app.use( '/', express.static( path.join(__dirname, '/client/public/') ) );
+app.use( '/', express.static( path.join(__dirname, '../client/public/') ) );
+
+app.get('/*', (req, res) => {
+  res.sendFile( path.join(__dirname, '../client/public/index.html'), err => {
+    err ? res.status(500).send(err) : null;
+  } )
+})
+
+const PORT = 1337;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
